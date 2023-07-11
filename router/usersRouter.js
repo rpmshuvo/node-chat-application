ab3f25cd491b72b222a28fdc72c24bb065ca3142
rpmshuvo <hasanshuvom884@gmail.com>
2023-07-12 01:08:37 +0600

    two

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,12 +2,26 @@
 const express = require("express");
 
 // internal imports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUploads");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
 // login page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+//add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
